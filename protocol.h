a7fe78e661f228ca46600dd66a330ab744bf9f71
Mathieu Laurendeau <mat.lau@laposte.net>
2017-04-06 18:59:03 +0200

    Use exact integral types.

--- protocol.h
+++ protocol.h
@@ -21,19 +21,19 @@ typedef enum {
 
 typedef struct PACKED
 {
-  unsigned char packet_type; // E_NETWORK_PACKET_IN_REPORT
-  unsigned char nbAxes; // number of elements in the following array
-  struct
+  uint8_t packet_type; // E_NETWORK_PACKET_IN_REPORT
+  uint8_t nbAxes; // number of elements in the following array
+  struct PACKED
   {
-    unsigned char index; // 1 MSB: 0 = relative axis, 1 = absolute axis, 7 LSB = index
-    int value;
+    uint8_t index; // 1 MSB: 0 = relative axis, 1 = absolute axis, 7 LSB = index
+    uint32_t value;
   } axes[0];
 } s_network_packet_in_report;
 
 typedef struct PACKED
 {
-  unsigned char packet_type; // E_NETWORK_PACKET_CONTROLLER
-  unsigned char controller_type;
+  uint8_t packet_type; // E_NETWORK_PACKET_CONTROLLER
+  uint8_t controller_type;
 } s_network_packet_controller;
 
 #endif /* GIMX_NETWORK_PROTOCOL_H_ */
